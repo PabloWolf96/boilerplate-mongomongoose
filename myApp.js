988a8980276af863f57d536673bd5b6f55b640f1
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-07 17:34:39 +0100

    setup mongodb

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to db");
+  });
 
 let Person;
 
