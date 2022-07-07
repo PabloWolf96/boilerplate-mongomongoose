008c766b769bb4e4b8efe08df6df1a524011a94a
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-07 22:36:31 +0100

    implement findAndUpdate

--- myApp.js
+++ myApp.js
@@ -95,8 +95,18 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, person) => {
+      if (err) {
+        done(err);
+      } else {
+        done(null, person);
+      }
+    }
+  );
 };
 
 const removeById = (personId, done) => {
